9e1eea6edd58f43823409963cef5c5fb6b85b4b7
Omar AlSuwaidi <omar.ali.alsuwaidi@gmail.com>
2023-12-28 05:08:26 +0400

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ وبه نستعين
+
+from flask import Flask, request, render_template, redirect, url_for, session
+from app_secrets import our_secret
+from datetime import timedelta
+import numpy as np
+
+# Initialize app:
+app = Flask(__name__, static_url_path='/static')
+
+# App config:
+app.secret_key = our_secret
+app.permanent_session_lifetime = timedelta(minutes=30)
+
+
+# Create landing page:
+@app.get('/')
+def homepage():
+    return render_template("HOMEPAGE.html", random=np.random.choice)
+
+
+# Create about page:
+@app.get('/about')
+def about():
+    return render_template("ABOUT.html")
+
+
+app.run(debug=True)
